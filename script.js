5c7f92e41c2b993eb67ef7d50c05aded8dda4a16
ninabuter <ninabbuter@gmail.com>
2025-10-13 22:58:59 +0200

    testing javascript

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-function navButton() {
-    document.getElementById("navButton").innerHTML = 'Hello JavaScript.';
-}
+// First test to include an JS element in my portfolio
+document.getElementById("date").textContent =
+    "Today is " + new Date().toDateString();
 
-// Test
-function myFunctioin() {
-    document.getElementById("demo1").innerHTML = "LOL";
-}
+// Dynamic greeting
+const hour = new Date().getHours()
+let message;
+if (hour < 12) message = "Good Morning";
+else if (hour < 18) message = "Good afternoon #1F31E";
+else message = "Good evening";
+
+let greeting = document.getElementById("greeting")
+greeting.textContent = message;
+
+// console.log("test")
