25faf193e58eb251844d28a089045db20acc8ba1
ninabuter <ninabbuter@gmail.com>
2025-10-13 09:20:18 +0200

    testing JS function

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function navButton() {
     document.getElementById("navButton").innerHTML = 'Hello JavaScript.';
 }
 
+// Test
+function myFunctioin() {
+    document.getElementById("demo1").innerHTML = "LOL";
+}
