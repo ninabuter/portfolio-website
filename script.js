0b8d0b91f3eef604ddf869f8596b230de287011a
ninabuter <ninabbuter@gmail.com>
2025-10-14 07:37:38 +0200

    added emoji's to greetings

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // First test to include an JS element in my portfolio
-document.getElementById("date").textContent =
-    "Today is " + new Date().toDateString();
+// document.getElementById("date").textContent =
+//     "Today is " + new Date().toDateString();
 
 // Dynamic greeting
 const hour = new Date().getHours()
 let message;
-if (hour < 12) message = "Good Morning";
-else if (hour < 18) message = "Good afternoon #1F31E";
-else message = "Good evening";
+if (hour < 12) message = "Good Morning \u{1F31E}";
+else if (hour < 18) message = "Good afternoon \u{1F308}";
+else message = "Good evening \u{1F31D}";
 
 let greeting = document.getElementById("greeting")
 greeting.textContent = message;
